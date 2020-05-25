0aa2fad4f3ce637c5c6d7f4db1d20f15340d0c98
Przemysl4wR4k <50297318+Przemysl4wR4k@users.noreply.github.com>
2020-05-25 12:51:28 +0200

    code commented

--- cpptry0/cpptry0/main.cpp
+++ cpptry0/cpptry0/main.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+// klasa zolw
 class Turtle {
 private:
 	int xPos;
@@ -49,6 +50,7 @@ void Turtle::setLifes(int n) {
 	lifes = n;
 }
 
+// klasa slomka
 class Straw {
 private:
 	int xPos;
@@ -85,44 +87,51 @@ void Straw::setYPos(int y) {
 	yPos = y;
 }
 
+// wektory sluzace do graficznego przedstawienia kolejno: zolwii, slomek, ramki gry
 vector <string> turtleInString;
 vector <string> strawInString;
 vector <string> frameInString;
+// wektory obiektow
 vector <Turtle> turtles;
 vector <Straw> straws;
 
+// deklaracja wystapienia dwoch zolwii
 Turtle player1(4, 10);
-
 Turtle player2(22, 29);
 
+//wielkosc ramki gry
 const int height = 40;
 const int width = 120;
 
+// zmienne opisujace szerokosc i wysokosc graficznego przedstawienia zolwia
 const int turtleWidth = 15;
 const int turtleHeight = 10;
 
+// zmienne opisujace szerokosc i wysokosc graficznego przedstawienia slomki
 const int strawWidth = 4;
 const int strawHeight = 10;
 
+// zmienne potrzebne do rozpoczecia generowania slomek
 int howManyStraws = 0;
 int oldIndexOfStrawToDel = -1;
 int indexOfStrawToDel = -1;
 
-clock_t start;
-//                __	__    _            __   __    _    ___ ____         __     ___ _____
-// \      / \  / /  \ |  \  / \  |       /  \ |  \  / \  /       /   |\  | |  \     |   | | 
-//  \    /   \/ |  __ |__/ |___| |      |  __ |__/ |___| |      /    | \ | |__/     |   | | 
-//   \/\/    /   \__/ |  \ |   | |___    \__/ |  \ |   | \___  / __  |  \| |  \    _|_ _|_|_ 
+
+clock_t start; // sluzy do liczenia czasu rozgrywki
+
+// sekcje krytyczne kolejno wyswietlania obrazu i obslugi ruchu gracza
 CRITICAL_SECTION paint_Section;
 CRITICAL_SECTION move_Section;
+
+// funkcja odpowiedzalna za wyswietlenie komunikatu o zakonczeniu gry
 void loadEnd(int losser) {
-	if (losser == 1)
+	if (losser == 1)	// w zaleznosci od tego kto zakonczyl gre, tj. kto stracil wszystkie zycia, zostaje wyswietlona informacja o zwyciestwie przeciwnika
 	{
 		std::cout << 
-			"                 __   __    _            __   __    _    ___ ____          __     ___ \n" <<
-			"  \\      / \\  / /  \\ |  \\  / \\  |       /  \\ |  \\  / \\  /       /   |\\  | |  \\     |  \n" <<
-			"   \\    /   \\/ |  __ |__/ |___| |      |  __ |__/ |___| |      /    | \\ | |__/     |  \n" <<
-			"    \\/\\/    /   \\__/ |  \\ |   | |___    \\__/ |  \\ |   | \\___ /___   |  \\| |  \\    _|_ \n"
+			"                 __   __    _            __   __    _    ___ ____          __     ___ \n" <<				//                __	__    _            __   __    _    ___ ____         __     ___ _____
+			"  \\      / \\  / /  \\ |  \\  / \\  |       /  \\ |  \\  / \\  /       /   |\\  | |  \\     |  \n" <<     // \      / \  / /  \ |  \  / \  |       /  \ |  \  / \  /       /   |\  | |  \     |   | | 
+			"   \\    /   \\/ |  __ |__/ |___| |      |  __ |__/ |___| |      /    | \\ | |__/     |  \n" <<            //  \    /   \/ |  __ |__/ |___| |      |  __ |__/ |___| |      /    | \ | |__/     |   | | 
+			"    \\/\\/    /   \\__/ |  \\ |   | |___    \\__/ |  \\ |   | \\___ /___   |  \\| |  \\    _|_ \n"			//   \/\/    /   \__/ |  \ |   | |___    \__/ |  \ |   | \___  / __  |  \| |  \    _|_ _|_|_
 		;
 		
 	}
@@ -139,6 +148,8 @@ void loadEnd(int losser) {
 	Sleep(10000);
 	exit(0);
 }
+
+// funkcja odpowiedzialna wczytanie "grafiki" przedstawiajacej zolwia do wektora
 void loadTurtle() {
 	turtleInString.push_back("      ___      ");
 	turtleInString.push_back("__   |   |   __");
@@ -152,6 +163,7 @@ void loadTurtle() {
 	turtleInString.push_back("/_/    v    \\_\\");
 }
 
+// funkcja odpowiedzialna wczytanie "grafiki" przedstawiajacej slomke do wektora
 void loadStraw() {
 	strawInString.push_back(" __ ");
 	strawInString.push_back("|  |");
@@ -165,6 +177,7 @@ void loadStraw() {
 	strawInString.push_back("|__|");
 }
 
+// funkcja odpowiedzialna wczytanie ramki gry do wektora
 void loadFrame() {
 
 	frameInString.clear();
@@ -187,29 +200,21 @@ void loadFrame() {
 	frameInString.push_back(topbottom);
 }
 
+// funkcja odpowiedzialna za naniesienie slomek i zolwii na ramke
 void __cdecl elementsToFrame( void *Args) {
 
-	EnterCriticalSection(&paint_Section);
+	EnterCriticalSection(&paint_Section);	//rozpoczecie sekcji ktytycznej majace na celu zapewnienie prawidlowego wyswietlania gry
 	loadFrame();
 
-
-	/* zajebiste debugowanie
-	cout << turtles.size()<<" size of turtles"<<endl;
-	cout << turtles.at(0).getXPos()<<" player1 X"<<endl;
-	cout << turtles.at(0).getYPos()<<" player1 Y"<<endl;
-	cout << turtles.at(1).getXPos()<<" player2 X"<<endl;
-	cout << turtles.at(1).getYPos()<<" player2 Y"<<endl;
-	*/
-
+	// naniesienie wszystkich zolwii
 	for (int i = 0; i < turtles.size(); i++) {
 		for (int j = 0; j < turtleHeight; j++)
 		{
 			frameInString.at(j + turtles.at(i).getYPos()).replace(turtles.at(i).getXPos(), turtleWidth, turtleInString.at(j));
-			//frameInString.at(j + player1.getYPos()).replace(player1.getXPos(), turtleWidth, turtleInString.at(j));
-			//frameInString.at(j + player2.getYPos()).replace(player2.getXPos(), turtleWidth, turtleInString.at(j));
 		}
 	}
 
+	// naniesienie slomek
 	for (int i = 0; i < straws.size(); i++) {
 		for (int j = 0; j < strawHeight; j++)
 		{
@@ -220,13 +225,13 @@ void __cdecl elementsToFrame( void *Args) {
 			}
 		}
 	}
-	LeaveCriticalSection(&paint_Section);
+	LeaveCriticalSection(&paint_Section);	//koniec sekcji krytyczne
 	_endthread();
 }
 
 /*
 void drawTurtle() {
-// tak cie widzą
+// zolw wyswietlony jako:
 	cout << "          ___		      " << endl;
 	cout << "    __   |   |   __      " << endl;
 	cout << "    \ \  |___|  / /      " << endl;
@@ -237,7 +242,7 @@ void drawTurtle() {
 	cout << "      / \/   \/ \		  " << endl;
 	cout << "     / / \___/ \ \		  " << endl;
 	cout << "    /_/    v    \_\	  " << endl;
-// tak cie piszą
+// bedzie zapisany jako:
 	cout << "          ___		      " << endl;
 	cout << "    __   |   |   __      " << endl;
 	cout << "    \\ \\  |___|  / /      " << endl;
@@ -250,7 +255,7 @@ void drawTurtle() {
 	cout << "    /_/    v    \\_\\	  " << endl;
 }
 void drawStraw() {
-// tak cie widzą
+// slomka wyswietlana jako
 	cout << "      __      	      " << endl;
 	cout << "      \ \			  " << endl;
 	cout << "       \ \			  " << endl;
@@ -261,7 +266,7 @@ void drawStraw() {
 	cout << "            \ \	  " << endl;
 	cout << "             \ \	  " << endl;
 	cout << "	       	   \_\	  " << endl;
-// tak cie piszą
+// bedzie zapisana jako
 	cout << "      __      	      " << endl;
 	cout << "      \\ \\			  " << endl;
 	cout << "       \\ \\			  " << endl;
@@ -274,21 +279,27 @@ void drawStraw() {
 	cout << "              \\_\\	  " << endl;
 }
 */
+
+// usuwanie slomek
 void deleteStraw(int index) {
 	straws.erase(straws.begin() + index);
 }
+
+// generowanie slomek
 void generateStraw() {
-	int number = rand() % 4;
+	int number = rand() % 20;	// tutaj wybieramy "zageszczenie" slomek / zasmiecenie zbornika wodnego, tym mozna regulowac poziom trudnosci gry
 	if (number == 1) {
-		int strawPosition = rand() % width;
-		Straw newStraw(strawPosition, -9, howManyStraws);
-		howManyStraws++;
-		straws.push_back(newStraw);
-		HANDLE hThread = (HANDLE) _beginthread( elementsToFrame, 0, NULL);
+		int strawPosition = rand() % width;					// losowanie umiejscowienia nowogenerowanej slomkki
+		Straw newStraw(strawPosition, -9, howManyStraws);	// nowa slomka ma wspolzedna Y = -9, zeby cala "schowala sie" na poczatku nad ramka gry i z niej dopiero spadala
+		howManyStraws++;				// licznik slomek
+		straws.push_back(newStraw);		// dodanie nowej slomki do wektora tychze obiektow
+		HANDLE hThread = (HANDLE) _beginthread( elementsToFrame, 0, NULL);	//naniesienie  nowych slomek na ekran
 	}
 }
 
+// mechanizm utraty zycia przez ktorego z graczy
 void isTurtleHurt() {
+	// dla kazdej slomki sprawdzamy, czy zajmowane przez nia pole nie koliduje z polem zajmowanym przez ktoregos z zolwii
 	for (int i = 0; i < straws.size(); i++) {
 		if ((straws.at(i).getXPos() >= turtles.at(0).getXPos()+1 && straws.at(i).getXPos() <= (turtles.at(0).getXPos() + turtleWidth - 1)) ||
 		   ((straws.at(i).getXPos() + strawWidth) >= turtles.at(0).getXPos()+1 && (straws.at(i).getXPos() + strawWidth) <= (turtles.at(0).getXPos() + turtleWidth -1)))
@@ -296,12 +307,13 @@ void isTurtleHurt() {
 			if ((straws.at(i).getYPos() >= turtles.at(0).getYPos()+2 && straws.at(i).getYPos() <= (turtles.at(0).getYPos() + turtleHeight -2)) ||
 				((straws.at(i).getYPos() + strawHeight) >= turtles.at(0).getYPos()+2 && (straws.at(i).getYPos() + strawHeight) <= (turtles.at(0).getYPos() + turtleHeight - 2)))
 			{
+				// jesli koliduje, to zolw traci zycie, zapisujemy tez ktora slomka go trafila
 				turtles.at(0).setLifes(turtles.at(0).getLifes() - 1);
-				//std::cout << "trafiony";
 				indexOfStrawToDel = straws.at(i).getId();
 				
 			}
 		}
+		// kazda slomka ma swoje id, jest to zabezpieczenie przed roznego rodzaju usuwaniem wiekszych ilosci slomek niz bysmy chcieli
 		if (oldIndexOfStrawToDel != indexOfStrawToDel)
 		{
 			oldIndexOfStrawToDel = indexOfStrawToDel;
@@ -316,7 +328,6 @@ void isTurtleHurt() {
 				((straws.at(i).getYPos() + strawHeight) >= turtles.at(1).getYPos() +2 && (straws.at(i).getYPos() + strawHeight) <= (turtles.at(1).getYPos() + turtleHeight -2)))
 			{
 				turtles.at(1).setLifes(turtles.at(1).getLifes() - 1);
-				//std::cout << "trafiony";
 				indexOfStrawToDel = straws.at(i).getId();
 			}
 		}
@@ -327,6 +338,7 @@ void isTurtleHurt() {
 			break;
 		}
 
+		// sprawdzamy czy gra powinna zostac zakoczona, czyli czy ktorys z graczy stracil wszystkie zycia, jesli tak, to konczymy gre
 		if (turtles.at(0).getLifes() <= 0)
 		{
 			loadEnd(0);
@@ -335,22 +347,20 @@ void isTurtleHurt() {
 		{
 			loadEnd(1);
 		}
-	/*	if ((turtles.at(0).getXPos() == i.getXPos()) || (turtles.at(0).getYPos() == i.getYPos())) {
-			turtles.at(0).setLifes(turtles.at(0).getLifes() - 1);
-		}
-		if ((turtles.at(1).getXPos() == i.getXPos()) || (turtles.at(1).getYPos() == i.getYPos())) {
-			turtles.at(1).setLifes(turtles.at(1).getLifes() - 1);
-		}*/
+
 	}
 }
 
+// funkcja odpowiadajaca za spadanie slomek o jedna pozycje w dol
 void strawMustFall() {
+	// obnizanie slomki
 	for (int i = 0; i < straws.size(); i++)
 	{
 		int oldY = straws.at(i).getYPos();
 		oldY++;
 		straws.at(i).setYPos(oldY);
 	}
+	// jesli slomka znajdzie sie pod ekranem gry, to ja usuwamy
 	for (int i = 0; i < straws.size(); i++)
 	{
 		if (straws.at(i).getYPos() > height) 
@@ -359,28 +369,30 @@ void strawMustFall() {
 			break;
 		}
 	}
+	// sprawdzamy czy slomki trafily zolwie
 	isTurtleHurt();
 
 }
 
 
-
+// funkcja odp za przemieszczanie sie zolwii
 void __cdecl input( void *Args) {
-	EnterCriticalSection(&move_Section);
+	EnterCriticalSection(&move_Section); // poczatek sekcji krytycznej ruchu
 
-		int oldX0 = turtles.at(0).getXPos();	//player1.getXPos();
-		int oldY0 = turtles.at(0).getYPos();	//player1.getYPos();
-		int oldX1 = turtles.at(1).getXPos();	//player2.getXPos();
-		int oldY1 = turtles.at(1).getYPos();	//player2.getYPos();
+		int oldX0 = turtles.at(0).getXPos();
+		int oldY0 = turtles.at(0).getYPos();
+		int oldX1 = turtles.at(1).getXPos();
+		int oldY1 = turtles.at(1).getYPos();
 
-		switch (_getch())
+		switch (_getch())	//sprawdzamy czy jakis klawisz zostal wcisniety
 		{
 		case 72:		//up
 			if (turtles.at(0).getYPos() > 1)
 			{
+				// warunku konieczne do spelnienia, aby dany ruch mogl sie odbyc
 				if ((( oldY0 - 1 > (oldY1 + turtleHeight)) || (oldX0 > oldX1 + turtleWidth )) || (oldX0 + turtleWidth < oldX1)) 
 				{
-					turtles.at(0).setYPos(oldY0 -= 1);	//player1.setYPos(oldY1 -= 3);
+					turtles.at(0).setYPos(oldY0 -= 1);	//player1.setYPos(oldY1 -= 3); w ten sposob mozna zmieniac tempo przemieszczania sie zolwii
 				}
 			}
 			break;
@@ -458,17 +470,17 @@ void __cdecl input( void *Args) {
 			}
 			break;
 		}
-		HANDLE hThread3 = (HANDLE)_beginthread(elementsToFrame, 0, NULL);
-	LeaveCriticalSection(&move_Section);
+		HANDLE hThread3 = (HANDLE)_beginthread(elementsToFrame, 0, NULL);	// narysowanie wprowadzonych zmian
+	LeaveCriticalSection(&move_Section);	// koniec sekcji krytycznej
 	_endthread();
 }
 
 int main()
 {
-	InitializeCriticalSection(&paint_Section);
-	InitializeCriticalSection(&move_Section);
+	InitializeCriticalSection(&paint_Section);	// sekcje krytyczne
+	InitializeCriticalSection(&move_Section);	
 
-	turtles.push_back(player1);
+	turtles.push_back(player1);	
 	turtles.push_back(player2);
 	srand(time(0));
 
@@ -478,7 +490,7 @@ int main()
 	loadFrame();
 	loadStraw();
 	HANDLE hThread = (HANDLE)_beginthread(elementsToFrame, 0, NULL);
-	start = clock();
+	start = clock();	// rozpoczecie odleiczania czasu
 
 	while (true)
 	{
@@ -489,11 +501,12 @@ int main()
 		{
 			HANDLE hThread2 = (HANDLE)_beginthread(input, 0, NULL);
 		}
-
+		
 		for (unsigned int i = 0; i < frameInString.size(); i++) {
 			std::cout << frameInString[i] << endl;
 		}
 
+		// informacje o czasie rozgrywki i ilosci zyc graczy
 		printf("Czas rozgrywki: %d s\n", ((clock() - start)) / 1000);
 		std::cout << "Zycia gracza nr1: " << turtles.at(0).getLifes() << endl;
 		std::cout << "Zycia gracza nr2: " << turtles.at(1).getLifes() << endl;
